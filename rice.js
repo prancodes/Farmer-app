4ffb1ac888492adc2d3c4fd62f7c5d79581ef4c1
Pranjal Singh <155365177+prancodes@users.noreply.github.com>
2024-08-11 19:39:14 +0530

    Add files via upload

---
+++ rice.js
@@ -0,0 +1,15 @@
+function back_btn(){
+    window.location.href = '/index.html';
+}
+
+let like = document.querySelectorAll('.like');
+like.forEach(heart => {
+    heart.addEventListener('click', function() {
+        if (this.src.includes('https://img.icons8.com/?size=100&id=85038&format=png&color=000000')) {
+            this.src = 'https://img.icons8.com/?size=100&id=84881&format=png&color=FA5252';
+        } else {
+            this.src = 'https://img.icons8.com/?size=100&id=85038&format=png&color=000000';
+        }
+    });
+});
+
